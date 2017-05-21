e3e19448cd96a996ee87773d2e1cd7c48b78a4ba
Thanakharn <Thanakharn1991@gmail.com>
2017-05-20 15:03:48 -1100

    Finish 20 May

--- app/src/main/java/app/alignsolution/mytraffic/MyData.java
+++ app/src/main/java/app/alignsolution/mytraffic/MyData.java
@@ -5,5 +5,23 @@ package app.alignsolution.mytraffic;
  */
 
 public class MyData {
-}    // Main Class
+
+    //Explicit  การประกาศตัวแปร
+    private String[] titleStrings = new String[] {"Title1","Title2","Title3",
+            "Title4","Title5","Title6","Title7","Title8","Title9","Title10",
+            "Title11","Title12","Title13","Title14","Title15","Title16","Title17",
+            "Title18","Title19","Title20"};
+
+    private String[] detailStrings;
+
+    public String[] getTitleStrings() {
+        return titleStrings;
+    }
+
+    public String[] getDetailStrings() {
+
+
+        return detailStrings;
+    }
+}    // Main Class คลาสหลัก
 
